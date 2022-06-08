c99d7254e0ff78284c158c197efa1e07a299c63d
Mohit Shyoran <mohitshyoran2014@gmail.com>
2022-06-08 23:22:33 +0530

    Update 1. Set Matrix Zeroes.cpp

--- Day 1: Arrays/1. Set Matrix Zeroes.cpp
+++ Day 1: Arrays/1. Set Matrix Zeroes.cpp
@@ -1,6 +1,36 @@
-#include<bits/stdc++.h>
-using namespace std;
-
-int main(){
-  return 0;
-}
+void setZeroes(vector<vector<int>>& A) {
+        int m = A.size();
+        int n = A[0].size();
+        bool isRowZero = false;
+        bool isColZero = false;
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
+                if(i == 0 && A[i][j] == 0){
+                    isRowZero = true;
+                }
+                if(j == 0 && A[i][j] == 0){
+                    isColZero = true;
+                }
+                if(A[i][j] == 0){
+                    A[i][0] = A[0][j] = 0;
+                }
+            }
+        }
+        for(int i = 1; i < m; i++){
+            for(int j = 1; j < n; j++){
+                if(A[i][0] == 0 || A[0][j] == 0){
+                    A[i][j] = 0;
+                }
+            }
+        }
+        if(isRowZero){
+            for(int i = 0; i < n; i++){
+                A[0][i] = 0;
+            }
+        }
+        if(isColZero){
+            for(int i = 0; i < m; i++){
+                A[i][0] = 0;
+            }
+        }
+    }
